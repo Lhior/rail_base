ee49a1b0d07c6da0191208c2e8668d83aa5814ee
Tianqing Zhang <ztq1996@live.com>
2024-07-12 10:53:56 -0700

    init commit for log_gp.py

---
+++ src/rail/estimation/algos/log_gp.py
@@ -0,0 +1,7 @@
+"""
+A summarizer that takes the input of a photo-z point estimate qp ensemble, and the cluster redshift likelihood
+and run logistic Gaussian process to estimate the posteroir of the redshift distribution n(z) for a sample. 
+
+Author: Markus Michael Rau, Tianqing Zhang
+"""
+
